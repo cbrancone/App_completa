7fa294c1c529153c81383947cb37fe70ad2f81f4
cbrancone <130234505+cbrancone@users.noreply.github.com>
2026-08-31 20:37:16 +0200

    Update app_completa.py

--- app_completa.py
+++ app_completa.py
@@ -71,8 +71,15 @@ authenticator = stauth.Authenticate(
 )
 
 # Mostra il form di login nella schermata principale
-name, authentication_status, username = authenticator.login(location="main", fields={"Form name": "Login"})
-if authentication_status == False:
+name, authentication_status, username = authenticator.login(
+    location="main", 
+    fields={
+        'Form name': 'Login', 
+        'Username': 'Username', 
+        'Password': 'Password', 
+        'Login': 'Login'
+    }
+)
     st.error("⚠️ Username o password errati.")
 elif authentication_status == None:
     st.warning("🔐 Inserisci le tue credenziali per accedere al gestionale.")
